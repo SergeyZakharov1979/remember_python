d5c549c90dd064b870f8f22500e59751d0f4f1e9
SergeyZakharov1979 <sergun.zahar79@gmail.com>
2024-12-29 13:17:47 +0700

    Четырёхзначное число

--- file.py
+++ file.py
@@ -1,3 +1,11 @@
-minutes = int(input())
+number = int(input())
 
-print(f'{minutes} мин - это {minutes // 60} час {minutes % 60} минут.')
+thousands = number // 1000
+hundreds = number % 1000 // 100
+tens = number % 100 // 10
+units = number % 10
+
+print(f'Цифра в позиции тысяч равна {thousands}')
+print(f'Цифра в позиции сотен равна {hundreds}')
+print(f'Цифра в позиции десятков равна {tens}')
+print(f'Цифра в позиции единиц равна {units}')
