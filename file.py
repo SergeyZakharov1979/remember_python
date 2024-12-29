8a8aa4f192b4a5b0fd65ad897e3e9ef477dfb2d0
SergeyZakharov1979 <sergun.zahar79@gmail.com>
2024-12-29 13:42:52 +0700

    Номер купе

--- file.py
+++ file.py
@@ -1,11 +1,4 @@
-number = int(input())
+place = int(input())
+coupe = abs(-place // 4)
 
-thousands = number // 1000
-hundreds = number % 1000 // 100
-tens = number % 100 // 10
-units = number % 10
-
-print(f'Цифра в позиции тысяч равна {thousands}')
-print(f'Цифра в позиции сотен равна {hundreds}')
-print(f'Цифра в позиции десятков равна {tens}')
-print(f'Цифра в позиции единиц равна {units}')
+print(coupe)
