e536fa942758b8368fc15a143825cba601fa1cd0
SergeyZakharov1979 <sergun.zahar79@gmail.com>
2025-01-02 12:01:46 +0700

    Пересечение отрезков

--- file.py
+++ file.py
@@ -1,16 +1,14 @@
-n = int(input())
+a1, b1, a2, b2 = int(input()), int(input()), int(input()), int(input())
 
-if n == 0:
-    print('зеленый')
-elif 1 <= n <= 10 or 19 <= n <= 28:
-    if n % 2 == 0:
-        print('черный')
-    else:
-        print('красный')
-elif 11 <= n <= 18 or 29 <= n <= 36:
-    if n % 2 == 0:
-        print('красный')
-    else:
-        print('черный')
-else:
-    print('ошибка ввода')
+if a2 < a1:
+    a1, a2 = a2, a1
+    b1, b2 = b2, b1
+
+if b1 < a2:
+    print('пустое множество')
+elif b1 == a2:
+    print(a2)
+elif a1 <= a2 and b1 <= b2:
+    print(a2, b1)
+elif a1 <= a2 and b1 >= b2:
+    print(a2, b2)
